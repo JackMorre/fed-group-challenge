d9e85ac0e53bf4ed0e33ae4b8977116a089e4e97
[C[B[A[A[A[A[D[C[C[C[C[C[C[C[C[C[CiLi01[D[D[C[C[C[B[D[D[D[D[D[D[D[D[D[D[Cilinca.pricop100@gmail.com[C[B[C[C[C[C[C[C[C <ilinca.pricop100@gmail.com>
2023-09-13 18:44:49 +0100

    iLi's stuff

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+// iLi's stuff
+let index = 0;
+displayImages();
+function displayImages() {
+  let i;
+  const images = document.getElementsByClassName("image");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  index++;
+  if (index > images.length) {
+    index = 1;
+  }
+  images[index-1].style.display = "block";
+  setTimeout(displayImages, 4000); // Change image every 3.7 seconds
+}
+// end of iLi's stuff
